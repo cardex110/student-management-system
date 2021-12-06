c9580db8b94267a51d5f07a7e60afb1d00d65afc
Amine Baba <ababa@rbbn.com>
2021-12-06 17:27:29 -0500

    initial commit

--- src/main/java/net/cardex/sms/controller/StudentController.java
+++ src/main/java/net/cardex/sms/controller/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import net.cardex.sms.entity.Student;
@@ -45,6 +46,30 @@ public class StudentController {
 	@PostMapping("/students")
 	public String saveStudent(@ModelAttribute("student") Student student) {
 		studentService.saveStudent(student);
+		return "redirect:/students";
+		
+	}
+	
+	@GetMapping("/students/edit/{id}")
+	public String editStudentForm(@PathVariable Long id, Model model) {
+		model.addAttribute("student", studentService.getStudentById(id));
+		return "edit_student";
+	}
+	
+	@PostMapping("/students/{id}")
+	public String updateStudent(@PathVariable Long id,
+			@ModelAttribute("student") Student student,
+			Model model) {
+		
+		//get student from database by id 
+		Student existingStudent = studentService.getStudentById(id);
+		existingStudent.setFirstName(student.getFirstName());
+		existingStudent.setLastName(student.getLastName());
+		existingStudent.setEmail(student.getEmail());
+		existingStudent.setId(id);
+		
+		//save updated student object
+		studentService.updateStudent(existingStudent);
 		return "redirect:/students";
 		
 	}
--- src/main/java/net/cardex/sms/service/StudentService.java
+++ src/main/java/net/cardex/sms/service/StudentService.java
@@ -8,4 +8,8 @@ public interface StudentService {
 	List<Student> getAllStudents();
 	
 	Student saveStudent(Student student);
+	
+	Student updateStudent(Student student);
+	
+	Student getStudentById(Long id);
 }
